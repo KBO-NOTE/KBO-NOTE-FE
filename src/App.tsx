35689c3037513c924eebfdda42865cc8f6f62707
양석준 <ysj7191@naver.com>
2026-01-20 10:14:15 +0900

    PR 문제점 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,23 @@
-import styled from 'styled-components';
-import './styles/App.css'
-import { QueryClientProvider } from "@tanstack/react-query";
+import styled from "styled-components";
 import "./styles/App.css";
-    
-import { ThemeProvider } from 'styled-components';
-import { theme } from './styles/theme';
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./styles/theme";
 import { QueryClient } from "@tanstack/react-query";
-import Router from "./routes";
-import {Outlet} from "react-router-dom"
+import { Outlet } from "react-router-dom";
 
 const queryClient = new QueryClient();
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
-    <MainContainer>
-      <Outlet />
-    </MainContainer>
-    </ThemeProvider>
+        <MainContainer>
+          <Outlet />
+        </MainContainer>
+      </ThemeProvider>
     </QueryClientProvider>
   );
 }
-
 
 const MainContainer = styled.div`
   width: 640px;
--- src/components/button/ButtonDefault.tsx
+++ src/components/button/ButtonDefault.tsx
@@ -1,34 +1,30 @@
-import styled from 'styled-components';
-import { theme } from '../../styles/theme';
+import styled from "styled-components";
+import { theme } from "../../styles/theme";
 
 interface ButtonProps {
-    buttonText: string;
-    onClick?: () => void;
+  buttonText: string;
+  onClick?: () => void;
 }
 
 const Button = ({ buttonText, onClick }: ButtonProps) => {
-    return (
-        <Container onClick={onClick}>
-            {buttonText}
-        </Container>
-    );
+  return <Container onClick={onClick}>{buttonText}</Container>;
 };
 
 const Container = styled.div`
-    width: 100%;
-    min-width: 362px;
-    height: 56px;
-    border-radius: ${theme.radius.full};
-    padding: 16px 48px 16px 48px;
-    gap: 10px;
-    background-color: ${theme.colors.primary400};
-    ${theme.typography.button01}
-    color: ${theme.colors.white};
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    cursor: pointer;
-    box-sizing: border-box;
+  width: 100%;
+  min-width: 362px;
+  height: 56px;
+  border-radius: ${theme.radius.full};
+  padding: 16px 48px 16px 48px;
+  gap: 10px;
+  background-color: ${theme.colors.primary400};
+  ${theme.typography.button01}
+  color: ${theme.colors.white};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  box-sizing: border-box;
 `;
 
 export default Button;
